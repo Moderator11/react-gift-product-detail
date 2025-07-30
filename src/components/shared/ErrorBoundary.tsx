2cc299ce50111cb2b5a093ec3e03957a63939709
Moderator11 <p7925221@gmail.com>
2025-07-30 15:55:01 +0900

    refact: Errorboundary fallback render logic simplified

--- src/components/shared/ErrorBoundary.tsx
+++ src/components/shared/ErrorBoundary.tsx
@@ -28,7 +28,7 @@ class ErrorBoundary extends React.Component<Props, State> {
 
   render() {
     if (this.state.hasError) {
-      return this.props.fallbackRender ? this.props.fallbackRender() : <></>;
+      return this.props.fallbackRender?.();
     }
 
     return this.props.children;
